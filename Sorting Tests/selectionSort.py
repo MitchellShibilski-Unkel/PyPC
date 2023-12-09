45c83017f3e574e54584920d8d8525f87e97b52e
DMInnovations <mitchellshibs@gmail.com>
2023-12-08 21:35:42 -0700

    Selection Sorting Algorithm/Test

---
+++ Sorting Tests/selectionSort.py
@@ -0,0 +1,30 @@
+import time
+
+
+# Start timer
+startTimer = time.perf_counter()
+startTimer2 = time.process_time()
+
+listOfNums = [None]
+
+sortedList = []
+def selectionSort(array = []):
+    i = 0
+    for arr in range(len(array)):
+        if i == len(array):
+            i = 0
+            break
+        else:
+            arrMin = min(array)
+            i += 1
+            sortedList.append(arrMin)
+            listOfNums.remove(arrMin)
+            
+selectionSort(listOfNums)
+
+print(sortedList)
+    
+endTimer = time.perf_counter()
+endTimer2 = time.process_time()
+
+print(f"Task Finished In Performance: {endTimer - startTimer:0.8f}\nTask Finished In Process/CPU/Kernel + User Space: {endTimer2 - startTimer2:0.8f}")
--- Sorting Tests/sortTest.py
+++ Sorting Tests/sortTest.py
@@ -6,7 +6,7 @@ startTimer = time.perf_counter()
 startTimer2 = time.process_time()
 
 # Random numbers in a list
-theList = [4, 5, 1, 3, 6, 10, 8, 9, 11, 7, 20, 22, 13, 17, 18, 12, 16, 71]
+theList = [None]
 
 # Run sort
 print(theList.sort())
