e86c0dff0825e8066d45cde48764da111e6f6cb6
DMInnovations <mitchellshibs@gmail.com>
2023-12-10 10:18:44 -0700

    Update computerInfo.py

--- General Computer Info/computerInfo.py
+++ General Computer Info/computerInfo.py
@@ -1,24 +1,48 @@
 import os
 import platform
+import psutil
+import numpy as np
 
 
 # CPU Core Count
 def cpuCoreCount():
-    print("Core Count: "+os.cpu_count())
+    print(f"Core Count: {os.cpu_count()}")
 
 # Processor/CPU Type  
 def processorType():
-    print("Processor: "+platform.processor())
+    print(f"Processor: {platform.processor()}")
 
 # OS Name
 def OS():
-    print("OS: "+platform.system())
+    print(f"OS: {platform.system()}")
     
 # Computer Architecture
 def computerArchitecture():
-    print("Architecture: "+platform.architecture())
+    print(f"Architecture: {platform.architecture()}")
+   
+# Physcial CPU Cores 
+def phyicalCoresCPU():
+    print(psutil.cpu_count(True))
+
+# Logical CPU Cores 
+def logicalCoresCPU():
+    print(psutil.cpu_count(False))
+
+# Overall CPU Usage
+def CPUsage():
+    print(f"CPU Usage: {psutil.cpu_percent()}%")
+ 
+# Overall RAM Usage   
+def RAMUsage():
+    ram = np.round(psutil.virtual_memory().used/1000000000, 1)
+    totalRAM = np.round(psutil.virtual_memory().total/1000000000, 2)
+    print(f"RAM Usage: {ram} GB / {totalRAM} GB")
     
 cpuCoreCount()
 processorType()
 OS()
 computerArchitecture()
+phyicalCoresCPU()
+logicalCoresCPU()
+CPUsage()
+RAMUsage()
