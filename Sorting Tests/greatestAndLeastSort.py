8816f67d1a3d30b1a2bf77ea7511bbc31a66b246
DMInnovations <133730558+DMInnovations@users.noreply.github.com>
2023-12-19 20:36:18 -0700

    Create greatestAndLeastSort.py

---
+++ Sorting Tests/greatestAndLeastSort.py
@@ -0,0 +1,52 @@
+import time
+
+
+# Start timer
+startTimer = time.perf_counter()
+startTimer2 = time.process_time()
+
+# Random numbers in a list
+theList = [None]
+
+# --- Merge Sort --- #
+sortedList = []
+def mergeSort(array = []):  
+    # --- Least to greatest --- #
+    # Get each side their vars  
+    leftSum = len(array) // 2
+    leftSide = array[0:leftSum]
+    rightSide = array[leftSum:]
+    
+    sortedArray = []
+
+    # Register values as a functional list
+    newRightSideList = []    
+    newLeftSideList = []
+    for nl in leftSide:
+        newLeftSideList.append(nl)
+    for rl in rightSide:
+        newRightSideList.append(rl)
+
+    # Sort left side
+    for l in range(len(newLeftSideList)):
+        minValue = min(newLeftSideList)
+        sortedArray.append(minValue)
+        newLeftSideList.remove(minValue)
+    
+    # Sort right side
+    for r in range(len(newRightSideList)):
+        minValue = min(newRightSideList)
+        sortedArray.append(minValue)
+        newRightSideList.remove(minValue)
+
+    # Sorted list
+    # Then, sort both sides using each sorted side (left and right) w/ the least values
+    print(sorted(sortedArray))
+    print(sorted(sortedArray, reverse=True))
+    
+mergeSort(theList)
+
+endTimer = time.perf_counter()
+endTimer2 = time.process_time()
+
+print(f"Task Finished In Performance: {endTimer - startTimer:0.8f}\nTask Finished In Process/CPU/Kernel + User Space: {endTimer2 - startTimer2:0.8f}")
